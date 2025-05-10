ebc6c9ab430b214d09306f109c586cd5a301aa60
Vicky Mahajan <110395736+VickyMahajanGitHub@users.noreply.github.com>
2025-05-10 12:45:19 +0530

    Support-page

--- src/Landing_page/support/SupportPage.js
+++ src/Landing_page/support/SupportPage.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import Navbar from '../Navbar';
+ 
 import Hero from './Hero';
 import CreateTicket from './CreateTicket';
 function SupportPage() {
     return ( 
         <>
-        
+        <Hero/>
+        <CreateTicket/>
         </>
      );
 }
