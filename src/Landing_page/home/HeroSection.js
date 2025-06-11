c33b71a4f6bb8445ab7e79184df4005cb4e51b70
Vicky Mahajan <110395736+VickyMahajanGitHub@users.noreply.github.com>
2025-06-11 17:23:48 +0530

    final-Commit

--- src/Landing_page/home/HeroSection.js
+++ src/Landing_page/home/HeroSection.js
@@ -10,7 +10,7 @@ function HeroSection() {
           width="85%"
           className="mb-5"
         />
-        <h1 classname="mt-5">Invest in Everything</h1>
+        <h1 className="mt-5">Invest in Everything</h1>
         <p>
           Online platform to invest in stocks, derivatives, mutual funds and
           more
